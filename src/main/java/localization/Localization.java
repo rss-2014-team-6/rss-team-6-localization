1d6d937cace86eedb9bb902cc08a54a8c99e3478
Gurtej Kanwar <gurtej@mit.edu>
2014-04-13 15:31:14 -0400

    Fixes map serialization

--- src/main/java/localization/Localization.java
+++ src/main/java/localization/Localization.java
@@ -6,6 +6,8 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.buffer.ChannelBuffer;
+import java.nio.ByteOrder;
 
 import map.PolygonMap;
 import rss_msgs.BumpMsg;
@@ -175,26 +177,20 @@ public class Localization implements NodeMain{
             e.printStackTrace();
             throw new RuntimeException("IOException on serializing map");
         }
-	/*
         MapMsg mapMsg = mapPub.newMessage();
 	byte[] ba = byteStream.toByteArray();
-	System.out.println("Byte stream byte array: " + ba);
-	for (int i = 0; i < ba.length; i++) {
-	    System.out.print(ba[i]);
-	}
-	System.out.print("\n");
-        mapMsg.setSerializedMap(ChannelBuffers.wrappedBuffer(byteStream.toByteArray()));
+	// ChannelBuffer MUST be little endian
+	ChannelBuffer buffer = ChannelBuffers.buffer(ByteOrder.LITTLE_ENDIAN, ba.length);
+	buffer.writeBytes(ba);
+        mapMsg.setSerializedMap(buffer);
         mapPub.publish(mapMsg);
-	*/
         // Publish the position estimate
 
-       	System.out.println("\n\nLOC part vals: " + bestParticle.getX() + ", " + bestParticle.getY() + ", " + bestParticle.getTheta());
         PositionMsg posMsg = posPub.newMessage();
         posMsg.setX(bestParticle.getX());
         posMsg.setY(bestParticle.getY());
         posMsg.setTheta(bestParticle.getTheta());
         posPub.publish(posMsg);
-	System.out.println("\n\n LOC msg vals: " + posMsg.getX() + ", " + posMsg.getY() + ", " + posMsg.getTheta());
     }
 
     // performs sensor updates based on sonar values for all particles
